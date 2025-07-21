672f9447493114fce27501c53895985769974f57
EOLindsay <eorleans_lindsay@st.ug.edu.gh>
2025-07-21 01:34:48 +0000

    Links

--- login.php
+++ login.php
@@ -105,11 +105,10 @@ $conn->close();
                             Login
                         </button>
                     </div>
-                    </form>
-                </div>
+                </form>
+            </div>
         </div>
     </div>
-    <h2>Login</h2>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
 </html>
